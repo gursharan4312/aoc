179bf5d8a1028989ac0897eb3c43221e91d0fa41
Gursharan Singh <gursharansingh4312@gmail.com>
2024-12-07 12:17:58 -0800

    day 7

---
+++ 2024/7/7.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+class Main {
+  public static void main(String args[]) throws IOException {
+    long sum = 0;
+    long sum2 = 0;
+    var lines = Files.lines(Paths.get("input.txt"))
+        .map(line -> line.replace(":", ""))
+        .map(line -> line.split("\\s+"))
+        .map(report -> Arrays.stream(report)
+            .mapToLong(Long::parseLong)
+            .toArray())
+        .toArray(long[][]::new);
+
+    for (long[] line : lines) {
+      if (canCalibrate(0, line[0], 1, line))
+        sum += line[0];
+    }
+
+    for (long[] line : lines) {
+      if (canCalibratePart2(0, line[0], 1, line))
+        sum2 += line[0];
+    }
+
+    System.out.println(sum);
+    System.out.println(sum2);
+  }
+
+  public static boolean canCalibrate(long currSum, long sum, int i, long[] line) {
+    if (currSum == sum && i == line.length)
+      return true;
+    if (currSum > sum || i >= line.length || line.length <= 1)
+      return false;
+    return canCalibrate(currSum + line[i], sum, i + 1, line)
+        || canCalibrate((i == 1 ? 1 : currSum) * line[i], sum, i + 1, line);
+  }
+
+  public static boolean canCalibratePart2(long currSum, long sum, int i, long[] line) {
+    if (currSum == sum && i >= line.length)
+      return true;
+    if (currSum > sum || i >= line.length || line.length <= 1)
+      return false;
+    return canCalibratePart2(currSum + line[i], sum, i + 1, line)
+        || canCalibratePart2((i == 1 ? 1 : currSum) * line[i], sum, i + 1, line)
+        || canCalibratePart2(Long.parseLong(("" + currSum + line[i])), sum, i + 1, line);
+  }
+}
